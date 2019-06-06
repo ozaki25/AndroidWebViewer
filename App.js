f8e269f0745447727ba70f98f805bbe00dddc5ca
ozaki <fs65ugumori@yahoo.co.jp>
2019-06-07 08:07:23 +0900

    save last url

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
-import React, { useEffect, useRef } from 'react';
-import { BackHandler, SafeAreaView, WebView } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { AsyncStorage, BackHandler, SafeAreaView, WebView } from 'react-native';
 
 function App() {
   const webviewEl = useRef(null);
+  const [uri, setUri] = useState('https://google.com');
+
+  const setupLastUrl = async () => {
+    try {
+      const lastUrl = await AsyncStorage.getItem('lastUrl');
+      setUri(lastUrl);
+    } catch (e) {
+      alert(e.toString());
+    }
+  };
 
   useEffect(() => {
     BackHandler.addEventListener('hardwareBackPress', () => {
@@ -11,13 +21,18 @@ function App() {
     });
   }, []);
 
+  useEffect(() => {
+    setupLastUrl();
+  }, []);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <WebView
-        source={{
-          uri: 'https://google.com/',
-        }}
+        source={{ uri }}
         ref={webviewEl}
+        onNavigationStateChange={({ url }) =>
+          AsyncStorage.setItem('lastUrl', url)
+        }
         style={{ flex: 1 }}
         startInLoadingState
       />
